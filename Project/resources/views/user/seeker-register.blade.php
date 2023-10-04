1608886c7b9f151cbb80aa40e5c0b12564fed7f7
Amine <wissarmohamedamine@gmail.com>
2023-10-04 03:50:10 -0700

    adding the UserController

---
+++ Project/resources/views/user/seeker-register.blade.php
@@ -0,0 +1,22 @@
+@extends("layouts.app")
+
+@section("content")
+<div class="controller">
+    <div class='row'>
+       <div class="col-md-6">
+        <h1>Looking for a job</h1>
+        <h3>Please creat an accont</h3>
+        <img src="{{asset('images/click.png')}}" alt="">
+       </div>
+
+       <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+                
+            </div>
+        </div>
+       </div>
+    </div>
+</div>
+@endsection
